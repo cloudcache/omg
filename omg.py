42b6426a4ff3c07e1f047ea35d10e02d208e7147
Richard Marshall <rgm@linux.com>
2011-07-29 00:38:34 -0700

    pylint inspired cleaning

--- omg.py
+++ omg.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+''' Frontend tool for omg '''
+
+# shut up pylint
+#pylint: disable=R0201
+#pylint: disable=C0103
+
 import sys
 import redis
 import shutil
@@ -11,22 +17,31 @@ sys.path.insert(0, BINPATH)
 import omg
 
 class Command(object):
+    ''' Command parent class '''
     def __init__(self):
         self.config = omg.config.Config(BINPATH)
         self.store = omg.store.Store()
 
     @classmethod
     def help(cls):
-        l = get_commands(cls)
-        for i in l:
-            args = "> <".join(get_args(getattr(cls, i)))
+        ''' Display help. '''
+        cmds = get_commands(cls)
+        name = cls.__name__.lower()
+        for cmd in cmds:
+            func = getattr(cls, cmd)
+            args = "> <".join(get_args(func))
             if args:
                 args = "<%s>" % args
-            print >> sys.stderr, '\t%s %s %s' % (cls.__name__.lower(), i, args)
+            if name == 'help':
+                cmds = ''
+            print >> sys.stderr, '    %s %s %s' % (name, cmd, args)
+            print >> sys.stderr, '        ', func.__doc__.strip()
 
 
 class Base(Command):
+    ''' Base image manager '''
     def create(self, name, path):
+        ''' Create base image. '''
         img = omg.volume.Volume()
         img['name'] = name
 
@@ -44,77 +59,93 @@ class Base(Command):
         shutil.copy(path, "%s/%s.img" % (self.config['base_path'], img.key))
 
     def list(self):
+        ''' List existing images. '''
         imgs = omg.images.Images('base')
         print imgs
 
     def delete(self, name):
+        ''' Delete a base image. '''
         print "name: ", name
 
 
 class Image(Command):
+    ''' Image manager '''
     pass
 
 
 class Vm(Command):
+    ''' Vm manager '''
     def list(self):
+        ''' List current vms. '''
         tpl = "{name:20}{uuid:40}{cpus:6}{ram:5}{vnc:8}{state:7}{ip:5}"
         vms = omg.vms.Vms('active')
         print tpl.format(name="NAME", uuid="UUID", cpus="CPUS", ram="RAM",
             vnc="VNC", state="STATE", ip="IP")
-        for k,v in vms.items():
-            t = {}
-            vm = omg.vm.VM(v)
+        for _, val in vms.items():
+            temp = {}
+            vm = omg.vm.VM(val)
             vm._load()
-            t['uuid'] = vm.key
-            t.update(vm.data)
-            t['vnc'] = ":%d" % (5900+int(t['vnc']))
-            if t['ip'] == 'None':
-                t['ip'] = 'DHCP'
-            print tpl.format(**t)
+            temp['uuid'] = vm.key
+            temp.update(vm.data)
+            temp['vnc'] = ":%d" % (5900+int(temp['vnc']))
+            if temp['ip'] == 'None':
+                temp['ip'] = 'DHCP'
+            print tpl.format(**temp)
 
     def create(self, name):
+        ''' Create a new vm. '''
         vm = omg.vm.VM()
         vm.create(name=name)
 
     def restart(self, name):
+        ''' Restart a vm. '''
         vm = omg.vm.VM(key=name)
         print vm.stop()
         print vm.start()
 
     def start(self, name):
+        ''' Start a vm. '''
         vm = omg.vm.VM(key=name)
         print vm.start()
 
     def stop(self, name):
+        ''' Stop a vm. '''
         vm = omg.vm.VM(key=name)
         print vm.stop()
 
     def destroy(self, name):
+        ''' Destroy a vm. '''
         vm = omg.vm.VM(key=name)
         print vm.destroy()
 
     def delete(self, name):
+        ''' Delete a vm. '''
         vm = omg.vm.VM(key=name)
         print vm.delete()
 
 
 class Config(Command):
+    ''' Config Manager '''
     def set(self, key, value):
+        ''' Set a configuration value. '''
         self.config[key] = value
         self.config.save()
         print self.config[key]
 
     def get(self):
+        ''' Get configuration values. '''
         print self.config
 
 
 class Help(Command):
+    ''' Help Manager '''
     @classmethod
     def help(cls):
-        print >> sys.stderr, '\thelp'
-        for k,v in COMMAND_MAP.items():
-            if k != 'help':
-                v.help()
+        ''' This help '''
+        super(Help, cls).help()
+        for key, val in COMMAND_MAP.items():
+            if key != 'help':
+                val.help()
 
 
 COMMAND_MAP = {
@@ -125,27 +156,35 @@ COMMAND_MAP = {
     'help': Help
 }
 
-def arg_count(fn):
-    t = inspect.getargspec(fn)
-    return len(t.args)-1
+def arg_count(func):
+    ''' Get number of arguments for a function '''
+    temp = inspect.getargspec(func)
+    return len(temp.args)-1
 
-def get_args(fn):
-    t = inspect.getargspec(fn)
-    return t.args[1:]
+def get_args(func):
+    ''' Get argument names '''
+    temp = inspect.getargspec(func)
+    return temp.args[1:]
 
 def get_commands(kls):
-    return [x for x in dir(kls) if x[0] != '_' and callable(getattr(kls,x))]
+    ''' Get class function names '''
+    return [x for x in dir(kls) if x[0] != '_' and callable(getattr(kls, x))]
 
-def help(msg, kls):
-    print >> sys.stderr, "ERROR:", msg
+def help(msg, kls, ret=1):
+    ''' Print help message '''
+    print >> sys.stderr, msg
     kls.help()
-    sys.exit(1)
+    sys.exit(ret)
 
 def main(argv):
+    ''' Program entry point '''
     if len(argv) < 1:
         help("Missing Command", Help)
 
     cmd = argv.pop(0)
+
+    if cmd == 'help':
+        help("Help", Help, 0)
 
     try:
         command = COMMAND_MAP[cmd]()
@@ -157,14 +196,14 @@ def main(argv):
             sub = argv.pop(0)
         except IndexError:
             help("Missing Action", command)
-        fn = getattr(command, sub)
-        c = len(argv)
-        a = arg_count(fn)
-        if c != a:
-            msg = "Not enough" if c < a else "Too many"
+        func = getattr(command, sub)
+        count = len(argv)
+        args = arg_count(fn)
+        if count != args:
+            msg = "Not enough" if count < args else "Too many"
             msg +="Arguments"
             help(msg, command)
-        fn(*argv)
+        func(*argv)
     except AttributeError:
         help("Unknown Action", Help)
 
